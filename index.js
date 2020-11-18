f1a2a15fae933265e6fca997f0dee66e52795c66
ericamar <ec0855797@otc.edu>
2020-11-17 18:39:36 -0600

    completed project

---
+++ index.js
@@ -0,0 +1,55 @@
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+
+
+//console logs static color text
+function printColor(string)
+{
+    console.log(chalk.blue(string));
+}
+
+//console logs rainbow text
+function printRainbow(string)
+{
+    const rainbow = chalkAnimation.rainbow(string);
+    rainbow.start();
+}
+
+
+//story begin -- using narration and dialogue
+
+printColor('\n\nOnce upon a time, there was a man who roamed the earth in search of answers.');
+
+printRainbow('Wḥ̶͍̏ͅò̴̹̗̳ ̴͓̼̔͠a̸͗ͅm̶̖͖͆ ̷̧̘̦̓̿͗I̶̘͐͜͠?̷͓͉̑̆');
+
+printColor('\nThe man had no memory of who he was or where he came from, but he felt like he could find others for help to find out who he is.');
+
+printRainbow('H̴̯͕̮͉̫̄̉̿͘e̸̠̳͚̜̓͂͒̑͝ĺ̸̫͇̤̿̕̚l̶̮̤̆̄͌ơ̸͉͈͔̞͓? C̴̖̿a̷̻̿ņ̵͠ ̸̫͐s̷͇̓o̸̭̅ṃ̴͂ê̸̘o̶͕͝n̸̢̊ĕ̵͉ ̷̥͐ḧ̶͎ę̸̅l̸̻̏p̵͎̍ ̶̮̽m̴̤̕é̵̝?̷̺̌');
+
+
+printColor('\nThe man visited a village and found a community of people. Even though he knew how to speak, the people he found had no idea what he was saying.');
+
+printRainbow('c̸̦̏a̸͚̋n̵̨̐ ̷̫͝ỷ̶̡o̸͓͘ǘ̴̜ ̵͇̍t̸̯̆ȩ̷̔l̵͓͊ĺ̵͚ ̷͘ͅm̴͈͝è̵͉ ̵̟̐ẅ̶̫h̴̫͠y̸̆͜ ̷̢͌i̶̝͠ ̷̱̔ḍ̸̇o̵̝͛n̴̮͗t̴̠̅ ̵̰̑k̵̤̎ņ̵̃o̶̼͐ẁ̵͍ ̵͈͊m̵̗̑y̵̝͠ș̵̄e̵̛̖ľ̸̲f̴͗ͅ?̶̧̅?̵͇͠?̴̘̉');
+
+printColor('\nThe man visited countless communities and traveled so far in hopes of finding someone who understands him. He even wished his reality was different. He wished he could talk like everyone else and have what everyone else has — and he wished he could change his past so that somehow he was not in this situation.');
+
+printRainbow('W̴̦͒h̷̨́̄̐ÿ̵̛͈͑ ̵̠͎̉̄̾͜d̵̬̓o̷̙̔̈́̓ë̵̞ś̸̞̤̖ ̸͇̘͑̂ň̴̡́õ̶̧̀́ ̵̬͈̕̕o̸̢̨͆͜ń̸̥̏̿e̷̱̩͊̐̎ ̴̤̜̓͂̋u̵̥̩̩͆ń̶̼̮͇̉̐d̴̙̙̦͆̈́ḙ̵̛̦̳̽r̸͈̅̉͑š̶̢͍̜t̶̩͉̘̅͌a̷̳̭͝n̵͋̈́ͅd̸̖̤̔̉ ̸̣̼̺̐́̌m̸̺͙̫̊ê̵͕̬.̶̘̯͗.̶̡̫͕͗͌.̵̢̬̔̔');
+
+
+printColor('\nUntil one day, he had a realization that his ability makes him unique — despite how other people may see it. He started spending more time alone in solitude, and also realized that the ability to voluntarily be alone is the ability to love oneself. ');
+
+printRainbow('W̷h̵y̶ ̴c̴a̶n̶t̶ ̵I̴ ̶f̶i̵t̴ ̴i̷n̵ ̸w̵i̶t̷h̴ ̸e̷v̶e̵r̴y̵o̵n̷e̸ ̴e̷l̷s̵e̷?̶ C̸̛̦ö̴̭û̵͕l̵̙̏d̴͖͆ ̶̱͝I̸̝͗ ̷͍̉b̶͓̎e̶͔̎ ̶̖́l̸̥͆i̷͇͌k̵̫̔e̵̳̊ ̴̹́ẗ̸̖́h̸͙͘i̵̫̎s̸̘͛ ̷͓̂f̶̖̒ö̶̞r̴̲̔ ̶̭̆a̴̖͒ ̷̙̅r̴̥͠ḙ̴̀a̸͖̾s̴̢͗o̶̭͐n̵̲͂?̵̱͠ ̶̻̍C̴̩̚ȁ̷͉n̴̦̍ ̶̫̄I̷͈͠ ̵̱̃t̶̢̏û̶̦r̷̲̔n̴̝͑ ̶̧͛t̵̞͑ĥ̴̫i̷̺͆s̷̆͜ ̵͕̏i̶͠ͅn̶̹͑t̶̙̄o̴͙̅ ̴̻̿s̶̝͋o̶̝͘m̴̨̚ȇ̷͇t̵̮̾h̵̬́i̶͈̾ṉ̸̎ǵ̸̥ ̷͉͋b̴̤̉ȩ̴͠ä̸̹́ṳ̵̚t̵̬̐ï̵̹f̴̖̎ù̴̹l̶͔͌ ̷̢̏ă̶̞n̵̝͠d̴̻̈ ̵͍̓m̴̝̾é̶̘ă̶̧n̴̜̕i̴̪͝n̶̜̈́g̸̞̉f̴̰͆ů̷̢l̶̹̏?̸̺̕');
+
+
+printColor('\nThe man used this time alone to make art out of his mess. He found this to be fulfilling and meaningful — and he even stopped fretting about his past and found himself more in the present.');
+printRainbow('T̶͔̂̾̎h̷̩̣̐͌͌̏e̷̢̢͍͚̋͐̉ ̶̖̾ò̵̡̗̳n̵̘̺͖̈̚l̴̈́̔̇̃͜ý̷̬̰̤̃ ̷̢̠͓͑b̶̩̹̣͈̋̔̚̚e̵̢̗̔̅̉͛ị̶̯̻̋́n̶̮̑͌̑͂g̵͎͚̲̬̍̊ ̷͔̈́̌͝w̷̯̼̔h̴̡̼̟̭̿̄̎̈ó̶̻͓̞̿ͅ ̶͇̹̓͌ć̴̬̠ą̴̙͉̂͊̔̉n̷̠̘̙͍̿͐̄ ̷̬͋̚ų̶͎̼̯͠ǹ̷̺̈́͛̃d̶̨̥̳̤͒̌e̴̜̣̹͎͋̓͌̃r̵̙͗s̵̘̤̹̾͠t̸̗̫̀ȁ̵̯̺̲ņ̴̣͚͑͒͝d̷̻̦̣̾̕ ̷͈̼̑͒m̵͎̈́e̴̖͙͛̒̆͛ ̷̢͕̻͇͗́͘i̶̙͚̣̘͑͆̍͗s̵̢̮̬͑̽ ̷̡̰̲̥̆̅̀t̵̰̮̽h̵̡̥́̈́e̵̫̙̼͋̐ ̸̛̟̳̮̌ç̶̖̬̏̂̈́r̶͈̥̆̈ḛ̵̻͙̬̀a̵̭̋͆͘t̷̗̫̺̂̽̚͠ǫ̶̬͖̫͆͠r̵̻̤̯͉͗͐ ̶͈̄à̴̪̘n̴̛̻̜̊̋̎d̴͓̈́́̈́̕ ̵̛̗͎͍͠͝ó̵̹̥̑̃b̵͉͛̈̏̚ş̶̥̯̹̐̏e̸̢͚̤̽̾͑͗͜r̵̞͎͒́̃͗v̴̬̳̀̆ě̵̟͈͕r̶̂͌̔̕͜');
+
+printColor('\n\nEventually, his art was noticed by other people, and he even made some friends! Despite the language barrier, his art transcended culture and allowed him to connect with others. \n\n');
+
+printRainbow('̴̻͙̫͈͂(̸̰͋́̂o̷̺̠̻̙͋̀͝b̸̨̙̳̲̚s̵͈̻̗̈́ê̸͎͔̩̮̓r̸͚͎͈͒̂̔v̶͖͎̄͂̌͘e̴̢̳̽̀̔)̵͈̄̈');
+printRainbow('̴̨̡̧̛͉̪̱̫̲̫̪͙̝̺̜̥̫͔̰̥͎̘̠͍̼͕͔͔̟̬̼̫͗͒̏̐̋̈̎̓̓̓́̔̓͂́̄̋̍͗̈̈́͐̂̑̂̅̄͛́̎̄͜͜͠͠͝͠͝ͅ(̸̡̨̢̧̖̘͇͉̦̮̺͙̞̹͙̬̬̫͕͖̞̞̣̩̗̞̜̳͈̔̇̄͒̍̉̿̆̿͌̏̄́̔͑̾͋̍̃̈͌̊̀͘͝c̸̢̡̛͚̭̙̙͍̖̹̫̞̋͆͒̊͋̆̈͂̈́̒͑̎̅̐͋͌́̆́͌̉͑̓̊́̑̋̃͆͂̿̓̃̽́̽̍͋͘̚͘͘r̷̢̡̧̛̭̖̦͚͔̖̝͚̱̙͓̼̲̥͚̠̩̟̰̦̩͇̣̱̼͇̲̥̝͚̙̜̮͕͈̝͉͉̖͔̤̘͍̙̭̓̍́̓̋͗͂̀͊̿̋̔̽̆́͒̄̕͜͝͠ͅͅͅe̷̢̧̨̧̢̧̨̢̢̛̘͕̹̲̻͍̗̣̜̻̠̼̗͙̠̗̥̝͚̥̠̳̖̙͍̤̫͖͓͕͔͙̙͚͖̟͈̟͙̫͇̹̭͈̐̀͐̑̽͗̆̂̆̀͋̇͑͂̃̾̏̏͊̾͒̐̎̽̚͘͜͝͝ͅa̴̡̨̧̧͕̰̠̘̬͉͓͍̦̥̼̘̪͕͕̰̥͔͔̲̼̞͕̰̰̠̪̜̘̟͈̖͍͈̖̜͕̪̣͔͌̎̈́̅͂̓́́̊͗̈́̉̐̊̆̂̓̓̽͛͛̿̔̋́̄̈́͊̃̌́͋͐̇̅̏̑̽̄̏͗́͊͗̀̕̕̚̚͜͠͝͝t̵̡̢̨̠̦̻͖̣͔͍̼̝̠̜̳̪̼̜̭̤̝̐̊̇̀́́̀͛̒̓̄̃̌͌̈́̿͒̔͑̌̏̀̌͑̈̓̐̃̈́̆͆͛͛̓̋͐͌̈́̓̆̃͘̚͜͠͠͝ẽ̵̡̢̢̡̛̛̛̗̹̥͔͍̮̹̭̯̱̲͍̭̯̱̟̜̠͕̼̹̜͕͈̞̲͍̘͉̘̖̲͇̾̓̈́́̐̔̆̒̍͆̄̓͗̿̃̓̋̓̇̒̀̈́̀̌͑̏͊̐̆͗̾̑̅̊̅̒̏͂̀͛̉̈̆̓͌̈́͘͠͝)̸̨̨̧̧̡̢̡̛̛̛͚͍̩̝̥̝̝̪̖̰͓̣̬͇̫̜̗̝͇̜̲͓̯̉́̄̀̽̌̓̓̈̇̏̿̿͆͒̑͑͛̀̀̄̈́́̈́͂̔͑̉̚͜͜͝͝');
+printRainbow('~̶~̷~̴~̵~̴~̴~̵~̷~̸~̴~̴~̶~̷ ̸ ̷a̷w̵ ̷y̵e̸a̸ ̵ ̶ ̴:̴)̸');
+
+printColor('\n\nHowever, most importantly of all, he felt connected to himself and his individuality.');
+ printRainbow('̷̛̟͙̐ț̸̓͝ḩ̶͑̉ē̵̡̪̭ ̵̧͙̔ǐ̵̢̝̮̈́n̷̖̂͝ḓ̷̢̅͆i̵̥̘̍v̵̨̟̜̈́i̴̹̾͂̉d̴̳̲̓͜ŭ̸̥͝a̶̫̞͇̓̎t̶̬͉͑͌̑î̴̜̃o̷̝̙͒̏n̶͇̗͒͝ ̴̻̲͑̈́p̷͖̦̦̀̒̚ȓ̸̹̱̬̒ȍ̴̧͕͛c̵̨̮͇͊̀ȩ̵͕̈s̶̙͖̋͂̀s̵̪͎̟̊   \n\n The End');
+
